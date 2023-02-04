cb777e174ecc93177fb037c6d982eaea03570dc4
Emre Esen <118935449+eesenn0@users.noreply.github.com>
2023-02-04 15:39:21 +0300

    Caput Draconis Password
     If the password is "Caput Draconis" the program prints "Welcome!". Otherwise, the program prints "Off with you!"

---
+++ Password.java
@@ -0,0 +1,21 @@
+import java.util.Scanner; // to use scanner
+
+public class Password { // Password class
+
+    public static void main(String[] args) { // main method
+        // create reader to take an input
+        Scanner reader = new Scanner(System.in);
+
+        // taking input
+        System.out.println("Password?");
+        String password = reader.nextLine();
+
+        // conditional statements
+        if (password.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
+
+    } // end of main method
+} // end of CaputDraconis class
