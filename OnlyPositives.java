15f69b25c0ec02983676195c5989abf9806c84d0
Emre Esen <118935449+eesenn0@users.noreply.github.com>
2023-02-04 23:38:01 +0300

    Only Positives

---
+++ OnlyPositives.java
@@ -0,0 +1,32 @@
+import java.util.Scanner; // to use scanner
+
+public class OnlyPositives { // OnlyPositives class
+
+    public static void main(String[] args) { // main method
+
+        // creating scanner to take a input
+        Scanner scanner = new Scanner(System.in);
+
+        // conditional statement
+        while (true) {
+            System.out.println("Give a number");
+            // take input from user
+            int number = Integer.valueOf(scanner.nextLine());
+            // conditional statements
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+                continue; // restart the execution
+            }
+            if (number == 0) {
+                break; // end to program
+            }
+            if (number > 0) {
+                // if the number is positive, then take square
+                System.out.println(number * number);
+                continue; // restart the execution
+            }
+            System.out.print(number + " ");
+        }
+
+    } // end of main method
+} // end of OnlyPositives class
